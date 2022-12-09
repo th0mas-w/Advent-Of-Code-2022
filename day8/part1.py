b287a2c2a79f2cc7ae9e6cabe4788c210355a7d2
th0mas-w <thomaswesthead@icloud.com>
2022-12-09 12:18:14 +0000

    day8 save

--- day8/part1.py
+++ day8/part1.py
@@ -1,4 +1,19 @@
 forest = []
-vi
-with open("input.txt","r") as file:
-    for line in file:
+visable = []
+
+with open("input.txt","r",encoding="UTF-8") as file:
+    for row in file:
+        row = row.split()[0]
+        print(row)
+        forest.append(list(row))
+        visable.append([0 for i in range(len(row))])
+
+
+WIDTH = len(forest[0])
+HEIGHT = len(forest)
+
+for i,row in enumerate(forest):
+    for j,tree in enumerate(row):
+        if visable[i][j] == 1:
+            continue
+
