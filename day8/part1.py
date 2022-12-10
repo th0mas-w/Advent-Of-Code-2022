5c06ad5aa1c873fddcc9e78c9475ee6563672224
th0mas-w <thomaswesthead@icloud.com>
2022-12-10 12:33:37 +0000

    day8 finished

--- day8/part1.py
+++ day8/part1.py
@@ -23,45 +23,9 @@ for i,row in enumerate(forest):
         if j == 0 or i == 0 or j == len(forest[0]) - 1 or i == len(forest) - 1:
             visable[i][j] = 1
             continue
-        # check to the right
-        failed = False
-        for neighbour in row[j:]:
-            if int(neighbour) > int(tree):
-                failed = True
-                break
-        if failed == False:
+        
+        if all(forest[i][x] < forest[i][j] for x in range(j)) or all(forest[i][x] < forest[i][j] for x in range(j+1, len(forest[0]))) or all(forest[y][j] < forest[i][j] for y in range(i)) or all(forest[y][j] < forest[i][j] for y in range(i,len(forest))):
             visable[i][j] = 1
-            continue
-
-        # Check to the left
-        failed = False
-        for neighbour in row[:j]:
-            if int(neighbour) > int(tree):
-                failed = True
-                break
-        if failed == False:
-            visable[i][j] = 1
-            continue
-
-        # Check to the top
-        failed = False
-        for neighbour_index in range(i,0,-1):
-            if int(forest[neighbour_index][j]) > int(tree):
-                failed = True
-                break
-        if failed == False:
-            visable[i][j] = 1
-            continue
-
-        # Check to the bottom
-        failed = False
-        for neighbour_index in range(i,len(forest)):
-            if int(forest[neighbour_index][j]) > int(tree):
-                failed = True
-                break
-        if failed == False:
-            visable[i][j] = 1
-            continue
 
 for i in range(len(forest)):
     for j in range(len(row)):
