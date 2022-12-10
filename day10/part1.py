1dde7c54888f2161998b3f6587eb25092b06d652
th0mas-w <thomaswesthead@icloud.com>
2022-12-10 15:03:15 +0000

    save

--- day10/part1.py
+++ day10/part1.py
@@ -1,3 +1,26 @@
 cycle = 0
 x = 1
 
+strengths = []
+
+commands = [line.strip() for line in open("input.txt","r").readlines()]
+
+for i in range(len(commands)):
+
+    cycle += 1
+    if commands[i] == "noop":
+        continue
+
+    if cycle in [20,60,100,140,180,220]:
+        strengths.append(x * cycle)
+
+    cycle += 1
+
+    x += int(commands[i].split()[1])
+
+
+print("x:",x)
+print("cycle:",cycle)
+
+print(strengths)
+print(sum(strengths))
